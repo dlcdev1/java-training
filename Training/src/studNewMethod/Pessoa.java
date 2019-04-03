6ca5cd2344eb2f442a6fef072b457a8617ac170d
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2019-04-03 18:06:40 -0300

    news methods lambda

---
+++ Training/src/studNewMethod/Pessoa.java
@@ -0,0 +1,13 @@
+package studNewMethod;
+
+public class Pessoa {
+  
+  private String nome;
+  private Integer idade;
+  
+  public Pessoa(String nome, Integer idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+}
--- Training/src/studNewMethod/testLambda.java
+++ Training/src/studNewMethod/testLambda.java
@@ -1,9 +1,23 @@
 package studNewMethod;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class testLambda {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    
+    List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);    
+    
+    //Implementação tradicional
+    
+    for(Integer n: list) {
+      System.out.println(n);
+    }
+    
+    System.out.println("Com implementação Lambada");
+    //Implementação com expressões lambda e StreamAPI
+    list.forEach(n-> System.out.println(n));   
 
   }
 
