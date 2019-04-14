f38f8605a2cb132d6bb8840ee23ff5f4c904b836
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2019-04-14 20:33:12 -0300

    new testes program

--- Training/src/studNewMethod/Pessoa.java
+++ Training/src/studNewMethod/Pessoa.java
@@ -10,4 +10,20 @@ public class Pessoa {
     this.idade = idade;
   }
 
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public Integer getIdade() {
+    return idade;
+  }
+
+  public void setIdade(Integer idade) {
+    this.idade = idade;
+  }
+
 }
--- Training/src/studNewMethod/testLambda.java
+++ Training/src/studNewMethod/testLambda.java
@@ -2,22 +2,31 @@ package studNewMethod;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class testLambda {
 
   public static void main(String[] args) {
-    
-    List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);    
-    
-    //Implementação tradicional
-    
-    for(Integer n: list) {
+
+    List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+
+    // Implementação tradicional
+
+    for (Integer n : list) {
       System.out.println(n);
     }
-    
-    System.out.println("Com implementação Lambada");
-    //Implementação com expressões lambda e StreamAPI
-    list.forEach(n-> System.out.println(n));   
+
+    System.out.println("Com implementação Lambda");
+    // Implementação com expressões lambda e StreamAPI
+    list.forEach(n -> System.out.println(n));
+
+    List<Pessoa> listaPessoas = Arrays.asList(new Pessoa("Joao", 32), new Pessoa("Antonio", 20),
+        new Pessoa("Maria", 18), new Pessoa("Angela", 30));
+
+    Stream<Pessoa> streamPessoas = listaPessoas.stream();
+
+    Integer somaIdade = streamPessoas.filter(soma -> soma.getNome().startsWith("A"))
+        .mapToInt(p -> p.getIdade()).sum();
 
   }
 
